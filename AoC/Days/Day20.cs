f6ff33fdb6db472d7b69e5e9ccab93435d5554c4
Darshan Patel <darshan@firststeps.software>
2016-12-21 17:37:58 +0000

    Day 20, part 1

--- AoC/Days/Day20.cs
+++ AoC/Days/Day20.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace AoC.Days
 {
@@ -9,14 +11,41 @@ namespace AoC.Days
 
         public string Part1()
         {
-            var answer = "Unknown";
-            return answer;
+            var blacklist = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            var sortedBlacklist = new List<Tuple<uint, uint>>();
+            foreach (var block in blacklist)
+            {
+                var parts = block.Split('-');
+                var lowerBound = UInt32.Parse(parts[0]);
+                var upperBound = UInt32.Parse(parts[1]);
+                sortedBlacklist.Add(new Tuple<uint, uint>(lowerBound, upperBound));
+            }
+
+            sortedBlacklist = sortedBlacklist.OrderBy(e => e.Item1).ThenBy(e => e.Item2).ToList();
+            var maxValue = UInt32.MaxValue;
+            for (uint i = 0; i <= maxValue; i++)
+            {
+                bool allowed = true;
+                foreach (var block in sortedBlacklist)
+                {
+                    if (i >= block.Item1 && i <= block.Item2)
+                    {
+                        allowed = false;
+                        break;
+                    }
+                }
+                if (allowed)
+                {
+                    return $"Lowest IP: {i}";
+                }
+            }
+
+            return "Unknown";
         }
 
         public string Part2()
         {
-            var answer = "Unknown";
-            return answer;
+            return $"Allowed IPs: {0}";
         }
     }
 }
