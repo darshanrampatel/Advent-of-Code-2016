96eaacc8daeafa7bec2eaf7f0650242384f75c0f
Darshan Patel <darshan@firststeps.software>
2016-12-22 11:29:14 +0000

    Day 20, part 2

--- AoC/Days/Day20.cs
+++ AoC/Days/Day20.cs
@@ -12,18 +12,18 @@ namespace AoC.Days
         public string Part1()
         {
             var blacklist = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
-            var sortedBlacklist = new List<Tuple<uint, uint>>();
+            var sortedBlacklist = new List<Tuple<long, long>>();
             foreach (var block in blacklist)
             {
                 var parts = block.Split('-');
-                var lowerBound = UInt32.Parse(parts[0]);
-                var upperBound = UInt32.Parse(parts[1]);
-                sortedBlacklist.Add(new Tuple<uint, uint>(lowerBound, upperBound));
+                var lowerBound = long.Parse(parts[0]);
+                var upperBound = long.Parse(parts[1]);
+                sortedBlacklist.Add(new Tuple<long, long>(lowerBound, upperBound));
             }
 
             sortedBlacklist = sortedBlacklist.OrderBy(e => e.Item1).ThenBy(e => e.Item2).ToList();
             var maxValue = UInt32.MaxValue;
-            for (uint i = 0; i <= maxValue; i++)
+            for (long i = 0; i <= maxValue; i++)
             {
                 bool allowed = true;
                 foreach (var block in sortedBlacklist)
@@ -45,7 +45,38 @@ namespace AoC.Days
 
         public string Part2()
         {
-            return $"Allowed IPs: {0}";
+            var blacklist = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            var sortedBlacklist = new List<Tuple<long, long>>();
+            foreach (var block in blacklist)
+            {
+                var parts = block.Split('-');
+                var lowerBound = long.Parse(parts[0]);
+                var upperBound = long.Parse(parts[1]);
+                sortedBlacklist.Add(new Tuple<long, long>(lowerBound, upperBound));
+            }
+            var validIPAddresses = 0;
+            sortedBlacklist = sortedBlacklist.OrderBy(e => e.Item1).ThenBy(e => e.Item2).ToList();
+            var maxValue = UInt32.MaxValue;
+            long i = 0;
+            var currentIp = sortedBlacklist.FirstOrDefault(block => i >= block.Item1 && i <= block.Item2);
+
+            while (i <= maxValue)
+            {
+                while (currentIp != null)
+                {
+                    i = currentIp.Item2 + 1;
+                    currentIp = sortedBlacklist.FirstOrDefault(block => i >= block.Item1 && i <= block.Item2);
+                }
+
+                if (i <= maxValue)
+                {
+                    validIPAddresses++;
+                    i++;
+                    currentIp = sortedBlacklist.FirstOrDefault(block => i >= block.Item1 && i <= block.Item2);
+                }
+            }
+
+            return $"Allowed IPs: {validIPAddresses}";
         }
     }
 }
